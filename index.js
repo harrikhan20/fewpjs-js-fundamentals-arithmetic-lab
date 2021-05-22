1ebb50297497410be7ca20ff43125c3e159049d3
harris <khanharris7@yahoo.com>
2021-05-21 21:32:06 -0500

    commit tree bb3569c138859f230c8d4a81ee9d5aef0dd45ae5

--- index.js
+++ index.js
@@ -1,2 +1,57 @@
 
 // Write your code here
+let num1 = 31, num2 = 2;
+
+function multiply(num1, num2){
+
+return num1 * num2;  
+
+}
+
+multiply = 62;
+
+
+
+
+
+
+
+   
+  let mynumber = 10
+
+function random(mynumber){
+   
+ console.log (Math.floor(math.random(mynumber) * 10)); 
+return (Number.isInteger(mynumber));
+}
+
+  
+
+
+
+
+
+
+
+
+
+
+
+let num3 = 19, num4 = 5;
+
+function mod(num3, num4){
+return num3 % num4;
+
+}
+
+mod = 4;
+
+
+
+
+function max(){
+
+
+}
+
+max = 20
